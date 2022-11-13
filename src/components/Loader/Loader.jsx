09ac496bd883c550535f199abb73a00b8a8015d6
Angelica Orozco <angorozcar@gmail.com>
2022-11-13 12:03:52 -0300

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './loader.css';
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='loader-container'>
+      <img src="http://pa1.narvii.com/6471/84f2f6a80c3719b2e1fea0e5416dd4cf00dcd002_00.gif" alt="pikachu loader"  className='loader'/>
+    </div>
   )
 }
 
